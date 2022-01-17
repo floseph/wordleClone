04c29028678077db2d6a660bff89ea0430e6e48e
Florian Regnery <floregnery97@gmail.com>
2022-01-17 01:13:05 +0100

    first steps

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const fs = require("fs")
+const fs = require("fs");
+const readLineSync = require("readline-sync");
 
-const words = JSON.parse(fs.readFileSync("fiveLetterWords.json"))
+const words = JSON.parse(fs.readFileSync("fiveLetterWords.json"));
 
-const randomInt = Math.floor(Math.random() * words.length)
-const randomWord = words[randomInt]
-console.log(randomWord)
+const randomInt = Math.floor(Math.random() * words.length);
+const randomWord = words[randomInt];
+console.log(randomWord);
+
+let guess = "";
+let guessCounter = 0;
+while (guessCounter > 6){
+  guess = readLineSync;
+  console.log(guess)
+}
+
