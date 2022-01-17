82cde3f7c937b3046583aefe49d53bdb7571af0d
Florian Regnery <floregnery97@gmail.com>
2022-01-17 01:06:19 +0100

    first steps

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello world")
+const fs = require("fs")
+
+const words = JSON.parse(fs.readFileSync("fiveLetterWords.json"))
+
+const randomInt = Math.floor(Math.random() * words.length)
+const randomWord = words[randomInt]
+console.log(randomWord)
